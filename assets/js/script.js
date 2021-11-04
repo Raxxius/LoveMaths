bec7f2e418af9953cf6b5ecdf39b7c36645a8abe
Raxxius <Raxxman@gmail.com>
2021-11-04 14:44:48 +0000

    fixed text in function addition question

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,7 +51,7 @@ function incrementWrongAnswer() {
 function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textconent = "+";
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
