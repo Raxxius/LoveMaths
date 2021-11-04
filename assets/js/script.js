977d307066b48b1f8ff50c2de04f58ddda507f04
Raxxius <Raxxman@gmail.com>
2021-11-04 16:13:23 +0000

    sfdsgsdffds

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     document.getElementById("answer-box").addEventListener("keydown", function(event) {
         if (event.key === "Enter") {
-            checkAnswer():
+            checkAnswer();
         }
     })
 
