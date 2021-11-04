a23ceb8b5df9ed78a97d0c83ab132cd8d8d612a8
Raxxius <Raxxman@gmail.com>
2021-11-04 13:34:19 +0000

    fixed bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     for (let button of buttons) {
         button.addEventListener("click", function() {
-            if (this.getAttribute("data-type" === "submit") {
+            if (this.getAttribute("data-type") === "submit") {
                 alert("You Clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
